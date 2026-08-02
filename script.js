a22bf0614c0b4a849ead623a19cc3f5af458fb31
samiranaa196-cell <samiranaa196@gmail.com>
2026-08-02 10:12:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Welcome to DARK X CYBER");
+document.body.innerHTML += `
+<div class="box">
+<h2>DARK X CYBER</h2>
+<p>System Online ⚡</p>
+<button onclick="startGame()">START GAME</button>
+</div>
+`;
+
+function startGame() {
+  alert("Game Loading...");
+}
